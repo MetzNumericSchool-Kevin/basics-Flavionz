e55d556ea0a18fee0169906d143ee6f32b20cdb9
Flavio <[200~flavio.terenzi@stagiairesmns.frflavio.terenzi@stagiairesmns.frgit config --global user.email flavio.terenzi@stagiairesmns.fr>
2025-02-10 14:49:45 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -5,3 +5,11 @@ let stockPotionSoin = 10;
 let prixPotionSoin = 30;
 let boutiqueOuverte = true;
 const monnaie = "🪙";
+
+//Affichage conditionnel
+
+if (boutiqueOuverte) {
+  console.log(`Bienvenue dans ${nomBoutique}, Aventurier ! 🎉`);
+} else {
+  console.log(`${nomBoutique} est fermée, revenez plus tard Aventurier ! 😴`);
+}
